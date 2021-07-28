46f09473228293c2d0dd341e46df01d6e11c2562
desiandrade <desi.andrade@hotmail.com>
2021-07-28 16:13:38 -0300

    Adicionando primeiro teste de integração

---
+++ src/test/java/br/com/letscode/java/starwarsapirest/rebeldes/RebeldesRestControllerTest.java
@@ -0,0 +1,35 @@
+package br.com.letscode.java.starwarsapirest.rebeldes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RebeldesRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void listarRebeldes() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/rebeldes"))
+                .andDo(print())
+                .andExpect(status().isOk())
+        .andExpect(content().string(containsString("desi")));
+    }
+
+
+
+}
